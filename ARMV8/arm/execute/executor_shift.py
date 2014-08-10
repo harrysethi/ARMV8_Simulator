3434e207114ca9df78e481714f4accf7050ec52b
harrysethi <sethi.harinder@gmail.com>
2014-08-09 21:10:16 -0400

    shift

--- ARMV8/arm/execute/executor_shift.py
+++ ARMV8/arm/execute/executor_shift.py
@@ -5,41 +5,58 @@ Created on Aug 8, 2014
 '''
 from arm.utils.utilFunc import setInstrFlag
 from arm.utils.mem import regFile
-from arm.utils import const
+from arm.utils import const, utilFunc
 
 
-def execAsr_r32(binary):
-    rdKey = int(binary[27:32],2)
-    rnKey = int(binary[22:27],2)
-    rmkey = int(binary[11:16],2)
-    imm16 = binary[11:27].zfill(const.REG_SIZE)
+
+#Helper function
+def getFields_r(binary):
+    rdKey = utilFunc.getRegKeyByStringKey(binary[27:32])
+    rnKey = utilFunc.getRegKeyByStringKey(binary[22:27])
+    rmKey = utilFunc.getRegKeyByStringKey(binary[11:16])
+    rnVal = regFile[rnKey]
+    rmVal = regFile[rmKey]
+    return rdKey, rnKey, rmKey, rnVal, rmVal
+
+#Helper function
+def finalize_r(rdKey, instr, rd):
     del regFile[rdKey]
-    regFile.insert(rdKey,imm16)
+    regFile.insert(rdKey, rd)
+    print instr
     setInstrFlag()
+
+def execAsr_r32(binary):    
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'ASR w'+rdKey+", w"+rnKey+", w"+rmKey
+    rd = '0'*32+utilFunc.asr(rnVal[32:64], int(rmVal[59:64],2))
+    finalize_r(rdKey, instr, rd)
                        
 def execLsl_r32(binary):
-    rdKey = int(binary[27:32],2)
-    rnKey = int(binary[22:27],2)
-    setInstrFlag()
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'LSL w'+rdKey+", w"+rnKey+", w"+rmKey
+    rd = '0'*32+utilFunc.lsl(rnVal[32:64], int(rmVal[59:64],2))
+    finalize_r(rdKey, instr, rd)
     
 def execLsr_r32(binary):
-    rdKey = int(binary[27:32],2)
-    rnKey = int(binary[22:27],2)
-    setInstrFlag()
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'LSR w'+rdKey+", w"+rnKey+", w"+rmKey
+    rd = '0'*32+utilFunc.lsr(rnVal[32:64], int(rmVal[59:64],2))
+    finalize_r(rdKey, instr, rd)
     
 def execAsr_r64(binary):
-    rdKey = int(binary[27:32],2)
-    imm16 = binary[11:27].zfill(const.REG_SIZE)
-    del regFile[rdKey]
-    regFile.insert(rdKey,imm16)
-    setInstrFlag()
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'ASR x'+rdKey+", x"+rnKey+", x"+rmKey
+    rd = utilFunc.asr(rnVal, int(rmVal[58:64],2))
+    finalize_r(rdKey, instr, rd)
                        
 def execLsl_r64(binary):
-    rdKey = int(binary[27:32],2)
-    rnKey = int(binary[22:27],2)
-    setInstrFlag()
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'LSL x'+rdKey+", x"+rnKey+", x"+rmKey
+    rd = utilFunc.lsl(rnVal, int(rmVal[58:64],2))
+    finalize_r(rdKey, instr, rd)
     
 def execLsr_r64(binary):
-    rdKey = int(binary[27:32],2)
-    rnKey = int(binary[22:27],2)
-    setInstrFlag()
+    rdKey, rnKey, rmKey, rnVal, rmVal = getFields_r(binary)
+    instr = 'LSR x'+rdKey+", x"+rnKey+", x"+rmKey
+    rd = utilFunc.lsr(rnVal, int(rmVal[58:64],2))
+    finalize_r(rdKey, instr, rd)
