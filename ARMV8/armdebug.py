86a7a7464d9834608743dbd76e6ccb5fe8130cb8
harrysethi <sethi.harinder@gmail.com>
2014-08-12 23:12:38 +0530

    minor chngs

--- ARMV8/armdebug.py
+++ ARMV8/armdebug.py
@@ -9,6 +9,7 @@ import parsehelper
 import utilFunc
 import decoder
 import mem
+from utilFunc import binaryToHexStr
 
 DEBUG_MODE=False
 PC = 0
@@ -332,7 +333,7 @@ def executeFlag():
 def executeRegs():
     i=0;
     for x in mem.regFile:
-        print 'Register'+str(i)+': '+hex(int(x,2))
+        print 'Register'+str(i)+': '+binaryToHexStr(x)
         i=i+1
     
 def printMainHelp():
