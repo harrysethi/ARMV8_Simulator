2249fe9f44595e9cd66431c6c4acb5aa3a249243
Abhishek <abhiagar90@gmail.com>
2014-08-16 00:13:17 +0530

    final commit of brnach cond

--- ARMV8/executor_branch.py
+++ ARMV8/executor_branch.py
@@ -7,34 +7,43 @@ Created on Aug 8, 2014
 import utilFunc
 
 def conditionHolds(bits_four):
+    #print 'condHolds'
     first_three=bits_four[0:3]
+    #print first_three
     result=False
     cond=''
     if first_three=='000':
         result=utilFunc.get_Z_flag()=='1'
         cond='EQ/NE'
+        #print '1'
     elif first_three=='001':
         result=utilFunc.get_C_flag()=='1'
         cond='CS/CC'
+        #print '2'
     elif first_three=='010':
         result=utilFunc.get_N_flag()=='1'
         cond='MI/PL'
+        #print '3'
     elif first_three=='011':
         result=utilFunc.get_V_flag()=='1'
         cond='VS/VC'
+        #print '4'
     elif first_three=='100':
         result=utilFunc.get_C_flag()=='1' and utilFunc.get_Z_flag()=='0'
         cond='HI/LS'
+        #print '5'
     elif first_three=='101':
-        result=utilFunc.get_N_flag()==utilFunc.get_V_flag()
+        result=(utilFunc.get_N_flag()==utilFunc.get_V_flag())
         cond='GE/LT'
+        #print '6'
     elif first_three=='111':
         result=True
         cond='AL'
+        #print'7'
         
     if bits_four[-1]=='1' and bits_four!='1111':
         result= not result
-        
+        #print '8'
     print cond
     return (result, cond)
 
@@ -55,13 +64,14 @@ def execB(binary):
     utilFunc.branchWithOffset(offset) #the magic!
     
 def execBCond(binary):
-    bits_four=binary[-4]
+    bits_four=binary[-4:]
+    #print 'bits four: '+bits_four
     xx=conditionHolds(bits_four)
     
     if not xx[0]:
         return
-    print xx[0]
-    print xx[1]
+    #print xx[0]
+    #print xx[1]
     inst ='B.'+xx[1]+' OFFSET('
     imm19key=binary[8:27]
     imm19key=utilFunc.signExtend(imm19key+'00', 64) #times 4 and 64 bits
@@ -103,14 +113,20 @@ def execCBZ_64(binary):
 def execCBNZ_64(binary):
     '''Not implemented yet'''
 
-def testB():
+'''def testB():
     binary=utilFunc.hexToBin('17fffffd')
     execB(binary)
     binary=utilFunc.hexToBin('14000002')
     execB(binary)    
 
 def testBCond():
-    binary=utilFunc.hexToBin('5400002e')
+    utilFunc.set_N_flag()
+    binary=utilFunc.hexToBin('54000024')
     execBCond(binary)
     
-testBCond()
+testBCond()'''
+'''
+utilFunc.set_Z_flag()
+utilFunc.printAllFlags()
+binary=utilFunc.hexToBin('5400002e')
+execBCond(binary)'''
--- ARMV8/main_test_a.py
+++ ARMV8/main_test_a.py
@@ -6,14 +6,16 @@ Created on Aug 8, 2014
 
 import decoder
 from utilFunc import resetInstrFlag, printAllRegs, printAllFlags
+import utilFunc
 
             
 def main():
     print "---Started---"
     #"52800102"
-    hexes = ["52800042","51000c43"]
+    hexes = ["5400002e","528000e2","17fffffe"]
     for hexcode in hexes:
         resetInstrFlag()
+        utilFunc.set_Z_flag()
         decoder.decodeInstr(hexcode)
     printAllRegs()
     printAllFlags()
